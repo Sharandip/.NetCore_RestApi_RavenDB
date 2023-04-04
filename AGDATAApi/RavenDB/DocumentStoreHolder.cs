f2c2cc50bb65241959a98ba442abe6e3da5c618c
Sharandip Kaur <sharandip.kaur@gmail.com>
2023-04-03 20:48:00 -0500

    Clean code

--- AGDATAApi/RavenDB/DocumentStoreHolder.cs
+++ AGDATAApi/RavenDB/DocumentStoreHolder.cs
@@ -6,8 +6,7 @@ namespace AGDATAApi.RavenDB
     // The `DocumentStoreHolder` class holds a single Document Store instance.
     public class DocumentStoreHolder
     {
-        // Use Lazy<IDocumentStore> to initialize the document store lazily. 
-        // This ensures that it is created only once - when first accessing the public `Store` property.
+
         private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
 
         public static IDocumentStore Store => store.Value;
@@ -16,24 +15,15 @@ namespace AGDATAApi.RavenDB
         {
             IDocumentStore store = new DocumentStore()
             {
-                // Define the cluster node URLs (required)
-                Urls = new[] { "http://192.168.56.1:8080/", 
-                           /*some additional nodes of this cluster*/ },
-
-                // Set conventions as necessary (optional)
+                Urls = new[] { "http://192.168.56.1:8080/",
+                            },
                 Conventions =
             {
                 MaxNumberOfRequestsPerSession = 10,
                 UseOptimisticConcurrency = true
             },
-
-                // Define a default database (optional)
                 Database = "AGDATA",
 
-                // Define a client certificate (optional)
-                //Certificate = new X509Certificate2("C:\\path_to_your_pfx_file\\cert.pfx"),
-
-                // Initialize the Document Store
             }.Initialize();
 
             return store;
